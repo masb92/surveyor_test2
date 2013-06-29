bd438ccac55971f446c9143d55859c55b6d604ce
Martin <masb92@gmail.com>
2013-06-29 14:45:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyorTest2::Application.config.secret_token = '2ab7b81807889117cea83a08e787d565af15068a693c32dfbf8937921d78683a232b2570e8d8ff17a3843de0d903172a230c8248b6e1f76ac2a42315cbb349bf'
