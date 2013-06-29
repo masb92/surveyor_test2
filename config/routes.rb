37af93b738a83981973818b5c3d50c9fa46dffb2
Martin <masb92@gmail.com>
2013-06-29 15:11:39 +0200

    Installed surveyor assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SurveyorTest2::Application.routes.draw do
+  mount Surveyor::Engine => "/surveys", :as => "surveyor"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
